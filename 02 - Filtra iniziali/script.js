ad2c9fce27719c15db03e9d2a70f6c6ec455745e
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-06 15:36:05 +0100

    exercise 02 - Filtra iniziali completed using declaring normal function and arrow function

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
-
+function nameFilter(arrayNames, char) {
+    // Array di appoggio
+    newArray = [];
+    for (let i = 0; i < arrayNames.length; i++) {
+        // assegno ad una variabile solo l'iniziale della stringa iterata così da poterla confrontare con la lettera iniziale
+        const initialChar = arrayNames[i].at(0);
+        if (initialChar.toLowerCase() === char.toLowerCase()) {
+            newArray.push(arrayNames[i]);
+        }
+    }
+    return newArray;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+result = nameFilter(names, 'a');
+console.log("Array contenenti parole con una certa iniziale: " + result);
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+/* Oppure usando un arrow function: */
+const resultArrow = names.filter((value) => value[0].toLowerCase() === 'a'.toLowerCase());
+console.log("Array contenenti parole con una certa iniziale (usando arrow function): " + resultArrow);
