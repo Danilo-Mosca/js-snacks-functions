688ae54605694ad6f6626deb609cda64abf294bc
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-06 17:07:49 +0100

    exercise 03 - conta vocali completed using declaring normal function and arrow function

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,36 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vowelsNumber(stringa) {
+    // Array contenente le vocali
+    const vowelsArray = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    // variabile contatore di vocali
+    let vocalCounter = 0;
+    for (let i = 0; i < stringa.length; i++) {
+        if (vowelsArray.includes(stringa[i])) {
+            vocalCounter++;
+        }
+    }
+    return vocalCounter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(vowelsNumber(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+/* Oppure usando un arrow function: */
+const result = (stringa) => {
+    // Array contenente le vocali
+    const vowelsArray = ['a', 'e', 'i', 'o', 'u, A, E, I, O, U'];
+    // variabile contatore di vocali
+    let vocalCounter = 0;
+    for (let i = 0; i < stringa.length; i++) {
+        if (vowelsArray.includes(stringa[i])) {
+            vocalCounter++;
+        }
+    }
+    return vocalCounter;
+}
+
+console.log("Risultato con la arrow function: " + result(word));
